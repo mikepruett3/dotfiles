048cea1ba65fc134699ce58121fff435604c74d1
Mike Pruett <mikepruett3@gmail.com>
2024-06-28 02:51:15 -0500

    Added missing tools to install script

--- run_once_install-packages.cmd
+++ run_once_install-packages.cmd
@@ -37,11 +37,6 @@ IF ERRORLEVEL 0 (
         START "Install Package - glow" /I /WAIT /B "sudo" "winget install charmbracelet.glow"
     )
 
-    WHERE /Q "mpv"
-    IF ERRORLEVEL 1 (
-        START "Install Package - mpv" /I /WAIT /B "sudo" "winget install mpv.net"
-    )
-
     WHERE /Q "mediainfo"
     IF ERRORLEVEL 1 (
         START "Install Package - mediainfo" /I /WAIT /B "sudo" "winget install MediaArea.MediaInfo"
@@ -63,6 +58,11 @@ IF ERRORLEVEL 0 (
     WHERE /Q "file"
     IF ERRORLEVEL 1 (
         START "Install Package - file" /I /WAIT /B "sudo" "scoop install file"
+    )
+
+    WHERE /Q "mpv"
+    IF ERRORLEVEL 1 (
+        START "Install Package - mpv" /I /WAIT /B "cmd.exe" "scoop winget install mpv.net"
     )
 
     WHERE /Q "chroma"
