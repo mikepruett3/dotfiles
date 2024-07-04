e1157e70a819935edd9aae79e8bdd527c13c7d22
Mike Pruett <mikepruett3@gmail.com>
2024-07-04 13:03:19 -0500

    Changing Windows Bootstrap scripts

--- dot_bootstrap/setup.bat
+++ dot_bootstrap/setup.bat
@@ -1,60 +1,89 @@
 @ECHO OFF
 
+GSUDO status IsElevated --no-output && GOTO :IsElevated
+
+ECHO Admin rights needed. Elevating using gsudo.
+GSUDO "%~f0" %*
+IF ERRORLEVEL 999 (
+    ECHO Failed to elevate!
+)
+EXIT /b %ERRORLEVEL%
+
+:winget
+ECHO.
+ECHO Installing Package - %~1
+START "Install Package - %~1" /I /WAIT /B winget.exe install %~1
+GOTO :IsElevated
+
+:scoop
+ECHO.
+ECHO Installing Package - %~1
+START "Install Package - %~1" /I /WAIT /B scoop install %~1
+GOTO :IsElevated
+
+:go
+ECHO.
+ECHO Installing Package - %~1
+START "Install Package - %~1" /I /WAIT /B go.exe install %~1
+GOTO :IsElevated
+
+:IsElevated
+
 WHERE /Q "winget"
 IF ERRORLEVEL 0 (
     WHERE /Q "starship"
     IF ERRORLEVEL 1 (
-        START "Install Package - Starship Prompt" /I /WAIT /B "sudo" "winget install Starship.Starship"
+        CALL :winget "Starship.Starship"
     )
 
     WHERE /Q "go"
     IF ERRORLEVEL 1 (
-        START "Install Package - GoLang" /I /WAIT /B "sudo" "winget install GoLang.Go"
+        CALL :winget "GoLang.Go"
     )
 
     WHERE /Q "bat"
     IF ERRORLEVEL 1 (
-        START "Install Package - bat" /I /WAIT /B "sudo" "winget install sharkdp.bat"
+        CALL :winget "sharkdp.bat"
     )
 
     WHERE /Q "less"
     IF ERRORLEVEL 1 (
-        START "Install Package - less" /I /WAIT /B "sudo" "winget install jftuga.less"
+        CALL :winget "jftuga.less"
     )
 
     WHERE /Q "lf"
     IF ERRORLEVEL 1 (
-        START "Install Package - lf" /I /WAIT /B "sudo" "winget install gokcehan.lf"
+        CALL :winget "gokcehan.lf"
     )
 
     WHERE /Q "eza"
     IF ERRORLEVEL 1 (
-        START "Install Package - eza" /I /WAIT /B "sudo" "winget install eza-community.eza"
+        CALL :winget "eza-community.eza"
     )
 
     WHERE /Q "glow"
     IF ERRORLEVEL 1 (
-        START "Install Package - glow" /I /WAIT /B "sudo" "winget install charmbracelet.glow"
+        CALL :winget "charmbracelet.glow"
     )
 
     WHERE /Q "mediainfo"
     IF ERRORLEVEL 1 (
-        START "Install Package - mediainfo" /I /WAIT /B "sudo" "winget install MediaArea.MediaInfo"
+        CALL :winget "MediaArea.MediaInfo"
     )
 
     WHERE /Q "mutool"
     IF ERRORLEVEL 1 (
-        START "Install Package - mutool" /I /WAIT /B "sudo" "winget install ArtifexSoftware.mutool"
+        CALL :winget "ArtifexSoftware.mutool"
     )
 
     WHERE /Q "fzf"
     IF ERRORLEVEL 1 (
-        START "Install Package - fzf" /I /WAIT /B "sudo" "winget install junegunn.fzf"
+        CALL :winget "junegunn.fzf"
     )
 
     WHERE /Q "age"
     IF ERRORLEVEL 1 (
-        START "Install Package - age" /I /WAIT /B "sudo" "winget install FiloSottile.age"
+        CALL :winget "FiloSottile.age"
     )
 )
 
@@ -62,17 +91,17 @@ WHERE /Q "scoop"
 IF ERRORLEVEL 0 (
     WHERE /Q "file"
     IF ERRORLEVEL 1 (
-        START "Install Package - file" /I /WAIT /B "sudo" "scoop install file"
+        CALL :scoop "file"
     )
 
     WHERE /Q "mpv"
     IF ERRORLEVEL 1 (
-        START "Install Package - mpv" /I /WAIT /B "sudo" "scoop install mpv"
+        CALL :scoop "mpv"
     )
 
     WHERE /Q "chroma"
     IF ERRORLEVEL 1 (
-        START "Install Package - chroma" /I /WAIT /B "sudo" "scoop install chroma"
+        CALL :scoop "chroma"
     )
 )
 
@@ -80,13 +109,8 @@ WHERE /Q "go"
 IF ERRORLEVEL 0 (
     WHERE /Q "ansiart2utf8"
     IF ERRORLEVEL 1 (
-        START "Install Package - ansiart2utf8" /I /WAIT /B "sudo" "go install github.com/BourgeoisBear/ansiart2utf8/ansiart2utf8@latest"
+        CALL :go "github.com/BourgeoisBear/ansiart2utf8/ansiart2utf8@latest"
     )
-
-    REM WHERE /Q "fli"
-    REM IF ERRORLEVEL 1 (
-    REM     START "Install Package - fil" /I /WAIT /B "sudo" "go install github.com/joeky888/fil@latest"
-    REM )
 )
 
-EXIT
+EXIT /b
