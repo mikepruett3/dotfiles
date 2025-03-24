d2a71c2d6a9e293c742147208662365b0eb15690
Mike Pruett <mikepruett3@gmail.com>
2025-03-23 23:10:22 -0500

    Ok, this should work!

--- dot_bootstrap/windows/ps1/Install-Packages.ps1
+++ dot_bootstrap/windows/ps1/Install-Packages.ps1
@@ -15,39 +15,34 @@ param ()
 
 $VerbosePreference = "Continue"
 
-begin {
-  Import-Module -Name $ENV:USERPROFILE\.bootstrap\windows\ps-functions\Install-WinGetPackage.ps1
 
-  $Packages = @(
-    "FiloSottile.age",
-    "sharkdp.bat",
-    "aristocratos.btop4win",
-    "cURL.cURL",
-    "waterlan.dos2unix",
-    "eza-community.eza",
-    "junegunn.fzf",
-    "Git.Git",
-    "charmbracelet.glow",
-    "GoLang.Go",
-    "GnuPG.GnuPG",
-    "jftuga.less",
-    "gokcehan.lf",
-    "Microsoft.VisualStudioCode",
-    "MinIO.Client",
-    "ArtifexSoftware.mutool",
-    "GNU.Nano",
-    "Neovim.Neovim",
-    "BurntSushi.ripgrep.MSVC",
-    "Starship.Starship"
-  )
+Import-Module -Name $ENV:USERPROFILE\.bootstrap\windows\ps-functions\Install-WinGetPackage.ps1
+
+$Packages = @(
+  "FiloSottile.age",
+  "sharkdp.bat",
+  "aristocratos.btop4win",
+  "cURL.cURL",
+  "waterlan.dos2unix",
+  "eza-community.eza",
+  "junegunn.fzf",
+  "Git.Git",
+  "charmbracelet.glow",
+  "GoLang.Go",
+  "GnuPG.GnuPG",
+  "jftuga.less",
+  "gokcehan.lf",
+  "Microsoft.VisualStudioCode",
+  "MinIO.Client",
+  "ArtifexSoftware.mutool",
+  "GNU.Nano",
+  "Neovim.Neovim",
+  "BurntSushi.ripgrep.MSVC",
+  "Starship.Starship"
+)
+
+foreach ($Package in $Packages) {
+  Install-WinGetPackage -PackageID $Package
 }
 
-process {
-  foreach ($Package in $Packages) {
-    Install-WinGetPackage -PackageID $Package
-  }
-}
-
-end {
-  Remove-Module -Name Install-WinGetPackage -ErrorAction SilentlyContinue
-}
+Remove-Module -Name Install-WinGetPackage -ErrorAction SilentlyContinue
