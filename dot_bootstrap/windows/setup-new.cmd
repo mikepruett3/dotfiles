6fa4e8426eb83085b4f5c9df6ddcc011f37c2c00
Mike Pruett <mikepruett3@gmail.com>
2025-03-23 23:30:21 -0500

    Changed Windows Bootstrap setup script to run some actions as Admin

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 REM Install package managers
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1" -Verb RunAs
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Verb RunAs
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1" -Verb RunAs
 
 REM Install Sudo package
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1" -Verb RunAs
 
 REM refreshenv
 
