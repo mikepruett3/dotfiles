f2aa8f890e43bd55fa1329108265ac4f33a1cde4
Mike Pruett <mikepruett3@gmail.com>
2025-03-23 23:46:30 -0500

    ????

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -2,13 +2,16 @@
 
 REM Install package managers
 REM PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1" -Verb RunAs
-PowerShell -Command 'Start-Process PowerShell -ArgumentList "-File '%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1' -Verb RunAs'
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Verb RunAs
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1" -Verb RunAs
+PowerShell -Command ^
+  'Start-Process PowerShell -ArgumentList `
+  "-File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1"" `
+  -Verb RunAs'
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Verb RunAs
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1" -Verb RunAs
 
 REM Install Sudo package
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1" -Verb RunAs
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1" -Verb RunAs
 
 REM refreshenv
 
-PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
+REM PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
