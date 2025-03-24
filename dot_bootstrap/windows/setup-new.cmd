d68a15c4be8b4b97f9bcfd33ef4479a15a8596a0
Mike Pruett <mikepruett3@gmail.com>
2025-03-24 00:22:11 -0500

    More Windows Bootstrap setup script updates

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -10,4 +10,5 @@ PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -Executi
 
 REM refreshenv
 
+PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-Packages.ps1"
 PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
