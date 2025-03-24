2f083ea27a25a36f81a3db61b32017651acf8c8c
Mike Pruett <mikepruett3@gmail.com>
2025-03-23 23:26:20 -0500

    Updated Install-WinGetPackage to include sudo functionality

--- dot_bootstrap/windows/ps-functions/Install-WinGetPackage.ps1
+++ dot_bootstrap/windows/ps-functions/Install-WinGetPackage.ps1
@@ -1,12 +1,16 @@
 function Install-WinGetPackage {
     <#
     .SYNOPSIS
-      Function that installs a winget package
+      Function that installs a WinGet package
     .DESCRIPTION
-      Install's a specific winget package, if its not already installed
+      Install's a specific WinGet package, if its not already installed
     .NOTES
       Author: Mike Pruett
       Date: March 23rd, 2025
+    .PARAMETER PackageID
+      WinGet Package ID to install
+    .PARAMETER Sudo
+      Wether or not to run WinGet as sudo
     .EXAMPLE
       > Install-WinGetPackage -PackageID xxx
     #>
@@ -15,16 +19,27 @@ function Install-WinGetPackage {
     param (
         [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
         [string]
-        $PackageID
+        $PackageID,
+        [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+        [switch]
+        $Sudo
     )
 
     if (!(winget list --id $PackageID)) {
+      if ($Sudo) {
+        Write-Verbose -Message "Install $PackageID..."
+        sudo winget install --silent `
+          --id $PackageID `
+          --accept-source-agreements `
+          --accept-package-agreements
+      } else {
         Write-Verbose -Message "Install $PackageID..."
         winget install --silent `
           --id $PackageID `
           --accept-source-agreements `
           --accept-package-agreements
+      }
     } else {
-        Write-Verbose -Message "$PackageID already installed!"
+      Write-Verbose -Message "$PackageID already installed!"
     }
 }
