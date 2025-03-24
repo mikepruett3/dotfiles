6e11173a2548db5c3db40659c4c36e6bf9f77e08
Mike Pruett <mikepruett3@gmail.com>
2025-03-24 00:15:57 -0500

    More Windows Bootstrap setup script cleanup

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -6,9 +6,8 @@ PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -Executi
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1"' -Verb RunAs"
 
 REM Install Sudo package
-REM PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1" -Verb RunAs
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1"' -Verb RunAs"
 
 REM refreshenv
 
-REM PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
+PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
