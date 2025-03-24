5bc0c63b4ae0841b8ef320c2ddc9d48dad049395
Mike Pruett <mikepruett3@gmail.com>
2025-03-23 21:37:42 -0500

    Added sudo package to Windows Bootstrap

---
+++ dot_bootstrap/windows/ps1/Install-Sudo.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+  Install's the Sudo package
+.DESCRIPTION
+  Download's and installs gerardog's sudo package, if its not already installed
+.NOTES
+  Author: Mike Pruett
+  Date: March 23rd, 2025
+.EXAMPLE
+  > Install-Sudo
+#>
+
+[CmdletBinding()]
+param ()
+
+Write-Verbose "Checking to see if sudo is already installed..."
+if (!(Get-Command -Name "sudo" -CommandType Application)) {
+  Write-Verbose "Installing sudo package manager..."
+  winget install --id gerardog.gsudo --source winget
+} else {
+  Write-Verbose "Sudo already installed!!!"
+}
--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -1,7 +1,13 @@
 @ECHO OFF
 
+REM Install package managers
 PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1" -Verbose
 PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Verbose
 PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1" -Verbose
+
+REM Install Sudo package
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1" -Verbose
+
+refreshenv
 
 PowerShell -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack" -Verbose
