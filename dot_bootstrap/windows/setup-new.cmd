3acc4429beae14b3f27611f31c5231ec8a2e77ef
Mike Pruett <mikepruett3@gmail.com>
2025-03-26 01:07:26 -0500

    Working changes to the Windows Bootstrap setup script

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -1,12 +1,9 @@
 @ECHO OFF
 
-REM Set the Execution Policy
-PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
-
 REM Install package managers, if missing
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1"' -Verb RunAs" -Wait
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1"' -Verb RunAs" -Wait
-PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Wait
+PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1"
 
 REM Install Sudo package, if missing
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1"' -Verb RunAs" -Wait
@@ -17,4 +14,7 @@ PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -Executi
 REM Installing Nerd Fonts, if missing
 PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
 
-REM REFRESHENV
+REM Set the Execution Policy
+PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
+
+REFRESHENV
