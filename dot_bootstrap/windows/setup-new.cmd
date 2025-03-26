35c543202862d5eb405fae3c166ab89dfe63eb1d
Mike Pruett <mikepruett3@gmail.com>
2025-03-26 01:00:20 -0500

    Another update to Windows Bootstrap setup script

--- dot_bootstrap/windows/setup-new.cmd
+++ dot_bootstrap/windows/setup-new.cmd
@@ -6,12 +6,12 @@ PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Cu
 REM Install package managers, if missing
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-WinGet.ps1"' -Verb RunAs" -Wait
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Choco.ps1"' -Verb RunAs" -Wait
-PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1" -Wait
+PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Scoop.ps1"
 
 REM Install Sudo package, if missing
 PowerShell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Sudo.ps1"' -Verb RunAs" -Wait
 
-REM refreshenv
+PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Packages.ps1"
+PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack"
 
-PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-Packages.ps1" -Wait
-PowerShell -ExecutionPolicy Bypass -File "%UserProfile%\.bootstrap\windows\ps1\Install-NerdFonts.ps1" -Name "Hack" -Wait
+REFRESHENV
