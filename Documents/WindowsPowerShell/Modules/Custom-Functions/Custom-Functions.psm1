cdff62fc5020d7933e69b38a22520785c422084c
Mike Pruett <mikepruett3@gmail.com>
2025-04-03 18:46:03 -0500

    This has to work!

--- Documents/WindowsPowerShell/Modules/Custom-Functions/Custom-Functions.psm1
+++ Documents/WindowsPowerShell/Modules/Custom-Functions/Custom-Functions.psm1
@@ -1,11 +1,8 @@
 #Dot source the files
-Foreach ($Import in @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -ErrorAction SilentlyContinue )) {
-    Try {
-        Import-Module $Import.FullName -Verbose
-    }
-    Catch {
-        Write-Error -Message "Failed to import function $($Import.FullName): $_"
-    }
+$Functions = @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -ErrorAction SilentlyContinue )
+
+foreach ($Function in @($Functions)) {
+    Import-Module $Function.FullName -Verbose
 }
 
 Export-ModuleMember -Function $Public.BaseName
