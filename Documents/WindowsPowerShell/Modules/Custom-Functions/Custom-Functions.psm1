c8b8f50271aef9d2a4c18dcf94ec1c8c877afd0f
Mike Pruett <mikepruett3@gmail.com>
2025-04-03 18:14:55 -0500

    Loosing it!

--- Documents/WindowsPowerShell/Modules/Custom-Functions/Custom-Functions.psm1
+++ Documents/WindowsPowerShell/Modules/Custom-Functions/Custom-Functions.psm1
@@ -3,7 +3,7 @@ Write-Output "$PSScriptRoot\Public\"
 
 Foreach ($Import in @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -ErrorAction SilentlyContinue )) {
     Try {
-        Import-Module $Import.FullName
+        Import-Module $Import.FullName -Verbose
     }
     Catch {
         Write-Error -Message "Failed to import function $($Import.FullName): $_"
