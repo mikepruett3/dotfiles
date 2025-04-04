7e95fda6ddba2f4c2872bc5c45145c71a5d914b3
Mike Pruett <mikepruett3@gmail.com>
2025-04-04 15:43:49 -0500

    Added custom shortcuts to PS Profile

--- dot_config/PowerShell/Shortcuts.ps1
+++ dot_config/PowerShell/Shortcuts.ps1
@@ -1,82 +1,95 @@
-# Windows Key Manager Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Key Manager.lnk" `
-                -App "$Env:SystemRoot\System32\rundll32.exe" `
-                -Arguments "keymgr.dll,KRShowKeyMgr" `
-                -Icon "%SystemRoot%\System32\imageres.dll, 77" `
-                -Description "Windows Key Manager" `
+# Setup AutoHotKey environment auto-start on login
+$Shell = New-Object -ComObject WScript.Shell
+$Shortcut = Join-Path -Path (Resolve-Path $Shell.SpecialFolders("startup")) -ChildPath "My AHK.lnk"
+if (-not (Test-Path -Path $Shortcut)) {
+    Create-Shortcut -Link $Shortcut `
+    -App "$ENV:UserProfile\.config\ahk\my_ahk.ahk" `
+    -Icon "$ENV:ProgramFiles\AutoHotkey\AutoHotkey.exe, 1" `
+    -Description "My AutoHotKey Launcher"
+}
+Remove-Variable -Name Shortcut -ErrorAction SilentlyContinue
 
-# Windows Credential Manager Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Credential Manager.lnk" `
-                -App "$Env:SystemRoot\System32\control.exe" `
-                -Arguments "/name Microsoft.CredentialManager" `
-                -Icon "%SystemRoot%\System32\imageres.dll, 54" `
-                -Description "Windows Credential Manager" `
+# Create Windows Key Manager Shortcut in Programs
+$Shortcut = Join-Path -Path (Resolve-Path $Shell.SpecialFolders("programs")) -ChildPath "Key Manager.lnk"
+if (-not (Test-Path -Path $Shortcut)) {
+    Create-Shortcut -Link $Shortcut `
+    -App "$ENV:SystemRoot\System32\rundll32.exe" `
+    -Arguments "keymgr.dll,KRShowKeyMgr" `
+    -Icon "$ENV:SystemRoot\System32\imageres.dll, 77" `
+    -Description "Windows Key Manager"
+}
+Remove-Variable -Name Shortcut -ErrorAction SilentlyContinue
+
+# Create Windows Credential Manager Shortcut
+$Shortcut = Join-Path -Path (Resolve-Path $Shell.SpecialFolders("programs")) -ChildPath "Credential Manager.lnk"
+if (-not (Test-Path -Path $Shortcut)) {
+    Create-Shortcut -Link $Shortcut `
+    -App "$ENV:SystemRoot\System32\control.exe" `
+    -Arguments "/name Microsoft.CredentialManager" `
+    -Icon "$ENV:SystemRoot\System32\imageres.dll, 54" `
+    -Description "Windows Credential Manager" `
+}
+Remove-Variable -Name Shortcut -ErrorAction SilentlyContinue
 
 # Windows Credential Backup Utility Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Credential Backup Utility.lnk" `
-                -App "$Env:SystemRoot\System32\credwiz.exe" `
-                -Icon "%SystemRoot%\System32\credwiz.exe" `
-                -Description "Windows Credential Backup Utility" `
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Credential Backup Utility.lnk" `
+#                -App "$Env:SystemRoot\System32\credwiz.exe" `
+#                -Icon "%SystemRoot%\System32\credwiz.exe" `
+#                -Description "Windows Credential Backup Utility" `
 
 # Administrative Tools Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk" `
-                -App "$Env:SystemRoot\System32\control.exe" `
-                -Arguments "/name Microsoft.AdministrativeTools" `
-                -Icon "%SystemRoot%\System32\imageres.dll, 109" `
-                -Description "Administrative Tools" `
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk" `
+#                -App "$Env:SystemRoot\System32\control.exe" `
+#                -Arguments "/name Microsoft.AdministrativeTools" `
+#                -Icon "%SystemRoot%\System32\imageres.dll, 109" `
+#                -Description "Administrative Tools" `
 
 # Active Directory Administration Center Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\dsac.lnk" `
-                -App "$Env:SystemRoot\System32\runas.exe" `
-                -Arguments "/noprofile /env /savecred /user:$Env:FAPS ""cmd /C Start /B %SYSTEMROOT%\System32\dsac.exe""" `
-                -Icon "%SystemRoot%\System32\dsac.exe" `
-                -Description "Active Directory Administration Center" `
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\dsac.lnk" `
+#                -App "$Env:SystemRoot\System32\runas.exe" `
+#                -Arguments "/noprofile /env /savecred /user:$Env:FAPS ""cmd /C Start /B %SYSTEMROOT%\System32\dsac.exe""" `
+#                -Icon "%SystemRoot%\System32\dsac.exe" `
+#                -Description "Active Directory Administration Center" `
 
 # Active Directory Users & Computers Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\ad.lnk" `
-                -App "$Env:SystemRoot\System32\runas.exe" `
-                -Arguments "/noprofile /env /savecred /user:$Env:FAPS ""cmd /C Start /B %SYSTEMROOT%\System32\mmc.exe %SYSTEMROOT%\System32\dsa.msc""" `
-                -Icon "%SystemRoot%\system32\dsadmin.dll, 0" `
-                -Description "Active Directory Users & Computers" `
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\ad.lnk" `
+#                -App "$Env:SystemRoot\System32\runas.exe" `
+#                -Arguments "/noprofile /env /savecred /user:$Env:FAPS ""cmd /C Start /B %SYSTEMROOT%\System32\mmc.exe %SYSTEMROOT%\System32\dsa.msc""" `
+#                -Icon "%SystemRoot%\system32\dsadmin.dll, 0" `
+#                -Description "Active Directory Users & Computers" `
 
 # Environment Variables Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Environment Variables.lnk" `
-                -App "$Env:SystemRoot\System32\rundll32.exe" `
-                -Arguments "sysdm.cpl,EditEnvironmentVariables" `
-                -Icon "%SystemRoot%\System32\shell32.dll, 189" `
-                -Description "Environment Variables" `
-                #-HotKey "CTRL+SHIFT+F"
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Environment Variables.lnk" `
+#                -App "$Env:SystemRoot\System32\rundll32.exe" `
+#                -Arguments "sysdm.cpl,EditEnvironmentVariables" `
+#                -Icon "%SystemRoot%\System32\shell32.dll, 189" `
+#                -Description "Environment Variables" `
+#                #-HotKey "CTRL+SHIFT+F"
 
-# Hosts File Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Hosts File.lnk" `
-                -App "$Env:SystemRoot\System32\notepad.exe" `
-                -Arguments "$Env:SystemRoot\System32\drivers\etc\hosts" `
-                -Icon "%SystemRoot%\System32\imageres.dll, 73" `
-                -Description "Edit the Hosts file with Notepad, running as Administrator" `
-                -Admin
+# Create Hosts File Shortcut
+$Shortcut = Join-Path -Path (Resolve-Path $Shell.SpecialFolders("programs")) -ChildPath "Hosts File.lnk"
+if (-not (Test-Path -Path $Shortcut)) {
+    Create-Shortcut -Link $Shortcut `
+    -App "$ENV:SystemRoot\System32\notepad.exe" `
+    -Arguments "$ENV:SystemRoot\System32\drivers\etc\hosts" `
+    -Icon "$ENV:SystemRoot\System32\imageres.dll, 73" `
+    -Description "Edit the Hosts file with Notepad, running as Administrator" `
+    -Admin
+}
+Remove-Variable -Name Shortcut -ErrorAction SilentlyContinue
 
-# System Restore Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\System Restore.lnk" `
-                -App "$Env:SystemRoot\System32\rstrui.exe" `
-                -Icon "%SystemRoot%\System32\rstrui.exe" `
-                -Description "Recover your system, using System Restore Point" `
-                -Admin
+## System Restore Shortcut
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\System Restore.lnk" `
+#                -App "$Env:SystemRoot\System32\rstrui.exe" `
+#                -Icon "%SystemRoot%\System32\rstrui.exe" `
+#                -Description "Recover your system, using System Restore Point" `
+#                -Admin
 
-# Create System Restore Point Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Create Instant System Restore Point.lnk" `
-                -App "$Env:UserProfile\dotposh\tools\Instant_Restore_Point.vbs" `
-                -Icon "%SystemRoot%\System32\bootux.dll, 20" `
-                -Description "Create Instant System Restore Point" `
-                -Admin
+## Create System Restore Point Shortcut
+#Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Create Instant System Restore Point.lnk" `
+#                -App "$Env:UserProfile\dotposh\tools\Instant_Restore_Point.vbs" `
+#                -Icon "%SystemRoot%\System32\bootux.dll, 20" `
+#                -Description "Create Instant System Restore Point" `
+#                -Admin
 
-# my_ahk.ahk Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\My AHK.lnk" `
-                -App "$Env:UserProfile\dotposh\ahk\my_ahk.ahk" `
-                -Icon "%UserProfile%\scoop\apps\autohotkey-installer\current\AutoHotkey.exe" `
-                -Description "My AutoHotKey Launcher"
-
-# SecureCRT Shortcut
-Create-Shortcut -Link "$Env:AppData\Microsoft\Windows\Start Menu\Programs\scrt.lnk" `
-                -App "$Env:LocalAppData\VanDyke Software\SecureCRT\SecureCRT.exe" `
-                -Icon "%LocalAppData%\VanDyke Software\SecureCRT\SecureCRT.exe" `
-                -Description "SecureCRT"
+Remove-Variable -Name Shell -ErrorAction SilentlyContinue
