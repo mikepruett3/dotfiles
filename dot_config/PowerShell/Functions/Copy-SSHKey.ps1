ced4f1f513184f04ff29d42ab5109920df03ac02
Mike Pruett <mikepruett3@gmail.com>
2025-04-04 10:21:32 -0500

    Rewriting Copy-SSHKey PS function

--- dot_config/PowerShell/Functions/Copy-SSHKey.ps1
+++ dot_config/PowerShell/Functions/Copy-SSHKey.ps1
@@ -3,7 +3,7 @@ function Copy-SSHKey {
     .SYNOPSIS
         Copy the SSH Public Key to a server
     .DESCRIPTION
-        Like the Unix equavilent, ssh-copy-id... this cmdlet will open
+        Like the Unix equivalent, ssh-copy-id... this cmdlet will open
         an SSH session to the server, and copy the contents of the SSH
         Public Key to the .ssh/authorized_keys on the remote server.
     .PARAMETER Server
@@ -14,90 +14,132 @@ function Copy-SSHKey {
         easier
     .PARAMETER Key
         Path to the SSH Public Key to copy to the server. Cmdlet will
-        use the Default Public Key $Env:UserProfile\.ssh\id_rsa.pub, 
-        if avaliable
+        use the Default Public Key $Env:UserProfile\.ssh\id_rsa.pub,
+        if available
     .EXAMPLE
         > Copy-SSHKey -Server myserver.example.com -Credentials myuser -Key \Path\To\Key\File.pub
     #>
+
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$True,ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
         [string]
         $Server,
 
-        [Parameter(Mandatory=$True,ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
         [string]
         $Credentials,
 
-        [Parameter(Mandatory=$False,ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
         [string]
         $Key
     )
-    
+
     begin {
-        # Load contents of SSH Public Key into variable
-        Write-Verbose "Load contents of SSH Public Key into variable..."
-        if ($Key -eq "") {
-            if (Test-Path -Path $Env:UserProfile\.ssh\id_rsa.pub) {
-                # Use the default location of the Public Key, if avaliable
-                $Key = "$Env:UserProfile\.ssh\id_rsa.pub"
+        Write-Verbose "Looking for a SSH Key in the default location..."
+        if (-not ($Key)) {
+            if (Test-Path -Path $ENV:UserProfile\.ssh\id_*.pub) {
+                Write-Verbose "Use the default location of the Public Key, if available"
+                $KeysFound = (Get-Item -Path $ENV:UserProfile\.ssh\id_*.pub).FullName
+                if (($KeysFound -match "ed25519") -and (-not ($Key))) {
+                    Write-Verbose "Located a ed25519 SSH Public key..."
+                    $Key = "$ENV:UserProfile\.ssh\id_ed25519.pub"
+                } elseif (($KeysFound -match "ecdsa") -and (-not ($Key))) {
+                    Write-Output "Located a ecdsa SSH Public key..."
+                    $Key = "$ENV:UserProfile\.ssh\id_ecdsa.pub"
+                } elseif (($KeysFound -match "rsa") -and (-not ($Key))) {
+                    Write-Output "Located a rsa SSH Public key..."
+                    $Key = "$ENV:UserProfile\.ssh\id_rsa.pub"
+                } elseif (($KeysFound -match "dsa") -and (-not ($Key))) {
+                    Write-Output "Located a dsa SSH Public key..."
+                    $Key = "$ENV:UserProfile\.ssh\id_dsa.pub"
+                }
             } else {
                 Write-Error "Key not found!!!"
                 Break
             }
-        } else {
+        }
+
+        Write-Verbose "Load contents of SSH Public Key into variable..."
+        try {
+            $SSHKey = Get-Content -Path $Key
+        }
+        catch {
             Write-Error "Key not found!!!"
             Break
         }
-        if (Test-Path -Path $Key) {
-            $SSHKey = Get-Content -Path "$Key"
-        }
-        
-        # Get Credentials into variable (User BetterCredentials to make this easier!)
+
+        # Use BetterCredentials cmdlet to make this easier!
         Write-Verbose "Load Credentials into variable..."
-        try { $Creds = Get-Credential("$Credentials") }
+        try {
+            $Creds = Get-Credential("$Credentials")
+        }
         catch {
             Write-Error "Could not retrieve the Credentials!!!"
             Break
         }
-        $UserName = $Creds.GetNetworkCredential().UserName
-        $Password = $Creds.GetNetworkCredential().Password
 
-        # Ping the server
+        Write-Verbose "Setting UserName variable..."
+        try {
+            $UserName = $Creds.GetNetworkCredential().UserName
+        }
+        catch {
+            Write-Error "Unable to set the UserName variable!!!"
+            Break
+        }
+
+        Write-Verbose "Setting Password variable..."
+        try {
+            $Password = $Creds.GetNetworkCredential().Password
+        }
+        catch {
+            Write-Error "Unable to set the Password variable!!!"
+            Break
+        }
+
         Write-Verbose "Testing communication with the $Server server..."
-        try { Test-Connection -ComputerName $Server -Count 1 -ErrorAction Stop > $null }
+        try {
+            Test-Connection -ComputerName $Server -Count 1 -ErrorAction SilentlyContinue
+        }
         catch {
             Write-Error "Count not communicate with $Server!!!"
             Break
         }
 
-        # Check for plink in system path
         Write-Verbose "Checking for plink in system path"
-        if (!(Get-Command -Name "plink.exe" -ErrorAction SilentlyContinue)) {
+        if (-not (Get-Command -Name "plink.exe" -ErrorAction SilentlyContinue)) {
             Write-Error "plink.exe either not installed, or not in system path!!!"
             Break
         }
-    }
-    
-    process {
-        try {
-            Write-Output "$SSHKey" | plink.exe "$Server" -l "$UserName" -pw "$Password" "umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"
-        }
-        catch {
-            Write-Error "Unable to copy the Public Key to the server!!!"
+
+        Write-Verbose "Checking for ssh in system path"
+        if (-not (Get-Command -Name ssh.exe -ErrorAction SilentlyContinue)) {
+            Write-Error "ssh.exe either not installed, or not in system path!!!"
             Break
         }
-        
     }
-    
+
+    process {
+        & plink.exe "$Server" -l "$UserName" -pw "$Password" "stat ~/.ssh"
+        #try {
+        #    Write-Output "$SSHKey" | plink.exe "$Server" -l "$UserName" -pw "$Password" "umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"
+        #}
+        #catch {
+        #    Write-Error "Unable to copy the Public Key to the server!!!"
+        #    Break
+        #}
+
+    }
+
     end {
         Write-Verbose "Cleaning up after Copying the SSH Public Key to the server $Server..."
-        Clear-Variable -Name Server -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name Credentials -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name Key -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name SSHKey -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name Creds -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name UserName -Scope Global -ErrorAction SilentlyContinue
-        Clear-Variable -Name Password -Scope Global -ErrorAction SilentlyContinue
+        Remove-Variable -Name Server -ErrorAction SilentlyContinue
+        Remove-Variable -Name Credentials -ErrorAction SilentlyContinue
+        Remove-Variable -Name Key -ErrorAction SilentlyContinue
+        Remove-Variable -Name KeysFound -ErrorAction SilentlyContinue
+        Remove-Variable -Name SSHKey -ErrorAction SilentlyContinue
+        Remove-Variable -Name Creds -ErrorAction SilentlyContinue
+        Remove-Variable -Name UserName -ErrorAction SilentlyContinue
+        Remove-Variable -Name Password -ErrorAction SilentlyContinue
     }
 }
