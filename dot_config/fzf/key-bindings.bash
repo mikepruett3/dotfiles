051f5e685e2de814b3fecd8e7db663433d287de4
Mike Pruett <mikepruett3@gmail.com>
2026-08-14 15:51:36 -0500

    Refactor Ctrl-H binding to use a custom history search function

--- dot_config/fzf/key-bindings.bash
+++ dot_config/fzf/key-bindings.bash
@@ -7,9 +7,11 @@ bind -r '\C-r'
 # Bind Ctrl-F for file picker
 bind -x '"\C-f": "fzf-file-widget"'
 
-# Bind Ctrl-H for history search
-bind -x '"\C-h": "fzf-history-widget"'
-
-# Customize history search options
-export FZF_CTRL_H_OPTS="--reverse --height 40%"
+# Create wrapper for history search and bind to Ctrl-H
+fzf-history-search() {
+  local cmd
+  cmd=$( (history -p ''; history) | fzf --reverse --height 40% --tac -n2..,..,.. +m)
+  eval "$cmd"
+}
+bind -x '"\C-h": "fzf-history-search"'
 
